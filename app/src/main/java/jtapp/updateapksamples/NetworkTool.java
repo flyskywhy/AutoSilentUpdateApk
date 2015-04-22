8d1642fe37c97e86ebc4173254392f00fedc2131
Li Zheng <flyskywhy@gmail.com>
2015-04-22 13:21:43 +0800

    Just CRLF to LF; Reformat Code

--- app/src/main/java/jtapp/updateapksamples/NetworkTool.java
+++ app/src/main/java/jtapp/updateapksamples/NetworkTool.java
@@ -1,8 +1,5 @@
 package jtapp.updateapksamples;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
@@ -11,33 +8,37 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class NetworkTool {
-	
-	/**
-	* 获取网址内容
-	* @param url
-	* @return
-	* @throws Exception
-	*/
-	public static String getContent(String url) throws Exception{
-	    StringBuilder sb = new StringBuilder();
-	    
-	    HttpClient client = new DefaultHttpClient();
-	    HttpParams httpParams = client.getParams();
-	    //设置网络超时参数
-	    HttpConnectionParams.setConnectionTimeout(httpParams, 3000);
-	    HttpConnectionParams.setSoTimeout(httpParams, 5000);
-	    HttpResponse response = client.execute(new HttpGet(url));
-	    HttpEntity entity = response.getEntity();
-	    if (entity != null) {
-	        BufferedReader reader = new BufferedReader(new InputStreamReader(entity.getContent(), "UTF-8"), 8192);
-	        
-	        String line = null;
-	        while ((line = reader.readLine())!= null){
-	            sb.append(line + "\n");
-	        }
-	        reader.close();
-	    }
-	    return sb.toString();
-	} 
+
+    /**
+     * 获取网址内容
+     *
+     * @param url
+     * @return
+     * @throws Exception
+     */
+    public static String getContent(String url) throws Exception {
+        StringBuilder sb = new StringBuilder();
+
+        HttpClient client = new DefaultHttpClient();
+        HttpParams httpParams = client.getParams();
+        //设置网络超时参数
+        HttpConnectionParams.setConnectionTimeout(httpParams, 3000);
+        HttpConnectionParams.setSoTimeout(httpParams, 5000);
+        HttpResponse response = client.execute(new HttpGet(url));
+        HttpEntity entity = response.getEntity();
+        if (entity != null) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(entity.getContent(), "UTF-8"), 8192);
+
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            reader.close();
+        }
+        return sb.toString();
+    }
 }
--- app/src/main/java/jtapp/updateapksamples/UpdateActivity.java
+++ app/src/main/java/jtapp/updateapksamples/UpdateActivity.java
@@ -1,19 +1,5 @@
 package jtapp.updateapksamples;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONArray;
-import org.json.JSONObject;
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -26,186 +12,200 @@ import android.os.Environment;
 import android.os.Handler;
 import android.util.Log;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class UpdateActivity extends Activity {
-	private static final String TAG = "Update";
-	public ProgressDialog pBar;
-	private Handler handler = new Handler();
+    private static final String TAG = "Update";
+    public ProgressDialog pBar;
+    private Handler handler = new Handler();
 
-	private int newVerCode = 0;
-	private String newVerName = "";
+    private int newVerCode = 0;
+    private String newVerName = "";
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		
-		if (getServerVerCode()) {
-			int vercode = Config.getVerCode(this);
-			if (newVerCode > vercode) {
-				doNewVersionUpdate();
-			} else {
-				notNewVersionShow();
-			}
-		}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
 
-	}
+        if (getServerVerCode()) {
+            int vercode = Config.getVerCode(this);
+            if (newVerCode > vercode) {
+                doNewVersionUpdate();
+            } else {
+                notNewVersionShow();
+            }
+        }
 
-	private boolean getServerVerCode() {
-		try {
-			String verjson = NetworkTool.getContent(Config.UPDATE_SERVER
-					+ Config.UPDATE_VERJSON);
-			JSONArray array = new JSONArray(verjson);
-			if (array.length() > 0) {
-				JSONObject obj = array.getJSONObject(0);
-				try {
-					newVerCode = Integer.parseInt(obj.getString("verCode"));
-					newVerName = obj.getString("verName");
-				} catch (Exception e) {
-					newVerCode = -1;
-					newVerName = "";
-					return false;
-				}
-			}
-		} catch (Exception e) {
-			Log.e(TAG, e.getMessage());
-			return false;
-		}
-		return true;
-	}
+    }
 
-	private void notNewVersionShow() {
-		int verCode = Config.getVerCode(this);
-		String verName = Config.getVerName(this);
-		StringBuffer sb = new StringBuffer();
-		sb.append("当前版本:");
-		sb.append(verName);
-		sb.append(" Code:");
-		sb.append(verCode);
-		sb.append(",\n已是最新版,无需更新!");
-		Dialog dialog = new AlertDialog.Builder(UpdateActivity.this)
-				.setTitle("软件更新").setMessage(sb.toString())// 设置内容
-				.setPositiveButton("确定",// 设置确定按钮
-						new DialogInterface.OnClickListener() {
+    private boolean getServerVerCode() {
+        try {
+            String verjson = NetworkTool.getContent(Config.UPDATE_SERVER
+                    + Config.UPDATE_VERJSON);
+            JSONArray array = new JSONArray(verjson);
+            if (array.length() > 0) {
+                JSONObject obj = array.getJSONObject(0);
+                try {
+                    newVerCode = Integer.parseInt(obj.getString("verCode"));
+                    newVerName = obj.getString("verName");
+                } catch (Exception e) {
+                    newVerCode = -1;
+                    newVerName = "";
+                    return false;
+                }
+            }
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+            return false;
+        }
+        return true;
+    }
 
-							@Override
-							public void onClick(DialogInterface dialog,
-									int which) {
-								finish();
-							}
+    private void notNewVersionShow() {
+        int verCode = Config.getVerCode(this);
+        String verName = Config.getVerName(this);
+        StringBuffer sb = new StringBuffer();
+        sb.append("当前版本:");
+        sb.append(verName);
+        sb.append(" Code:");
+        sb.append(verCode);
+        sb.append(",\n已是最新版,无需更新!");
+        Dialog dialog = new AlertDialog.Builder(UpdateActivity.this)
+                .setTitle("软件更新").setMessage(sb.toString())// 设置内容
+                .setPositiveButton("确定",// 设置确定按钮
+                        new DialogInterface.OnClickListener() {
 
-						}).create();// 创建
-		// 显示对话框
-		dialog.show();
-	}
+                            @Override
+                            public void onClick(DialogInterface dialog,
+                                                int which) {
+                                finish();
+                            }
 
-	private void doNewVersionUpdate() {
-		int verCode = Config.getVerCode(this);
-		String verName = Config.getVerName(this);
-		StringBuffer sb = new StringBuffer();
-		sb.append("当前版本:");
-		sb.append(verName);
-		sb.append(" Code:");
-		sb.append(verCode);
-		sb.append(", 发现新版本:");
-		sb.append(newVerName);
-		sb.append(" Code:");
-		sb.append(newVerCode);
-		sb.append(", 是否更新?");
-		Dialog dialog = new AlertDialog.Builder(UpdateActivity.this)
-				.setTitle("软件更新")
-				.setMessage(sb.toString())
-				// 设置内容
-				.setPositiveButton("更新",// 设置确定按钮
-						new DialogInterface.OnClickListener() {
+                        }).create();// 创建
+        // 显示对话框
+        dialog.show();
+    }
 
-							@Override
-							public void onClick(DialogInterface dialog,
-									int which) {
-								pBar = new ProgressDialog(UpdateActivity.this);
-								pBar.setTitle("正在下载");
-								pBar.setMessage("请稍候...");
-								pBar.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-								downFile(Config.UPDATE_SERVER
-										+ Config.UPDATE_APKNAME);
-							}
+    private void doNewVersionUpdate() {
+        int verCode = Config.getVerCode(this);
+        String verName = Config.getVerName(this);
+        StringBuffer sb = new StringBuffer();
+        sb.append("当前版本:");
+        sb.append(verName);
+        sb.append(" Code:");
+        sb.append(verCode);
+        sb.append(", 发现新版本:");
+        sb.append(newVerName);
+        sb.append(" Code:");
+        sb.append(newVerCode);
+        sb.append(", 是否更新?");
+        Dialog dialog = new AlertDialog.Builder(UpdateActivity.this)
+                .setTitle("软件更新")
+                .setMessage(sb.toString())
+                        // 设置内容
+                .setPositiveButton("更新",// 设置确定按钮
+                        new DialogInterface.OnClickListener() {
 
-						})
-				.setNegativeButton("暂不更新",
-						new DialogInterface.OnClickListener() {
-							public void onClick(DialogInterface dialog,
-									int whichButton) {
-								// 点击"取消"按钮之后退出程序
-								finish();
-							}
-						}).create();// 创建
-		// 显示对话框
-		dialog.show();
-	}
+                            @Override
+                            public void onClick(DialogInterface dialog,
+                                                int which) {
+                                pBar = new ProgressDialog(UpdateActivity.this);
+                                pBar.setTitle("正在下载");
+                                pBar.setMessage("请稍候...");
+                                pBar.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                                downFile(Config.UPDATE_SERVER
+                                        + Config.UPDATE_APKNAME);
+                            }
 
-	void downFile(final String url) {
-		pBar.show();
-		new Thread() {
-			public void run() {
-				HttpClient client = new DefaultHttpClient();
-				HttpGet get = new HttpGet(url);
-				HttpResponse response;
-				try {
-					response = client.execute(get);
-					HttpEntity entity = response.getEntity();
-					long length = entity.getContentLength();
-					InputStream is = entity.getContent();
-					FileOutputStream fileOutputStream = null;
-					if (is != null) {
+                        })
+                .setNegativeButton("暂不更新",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,
+                                                int whichButton) {
+                                // 点击"取消"按钮之后退出程序
+                                finish();
+                            }
+                        }).create();// 创建
+        // 显示对话框
+        dialog.show();
+    }
 
-						File file = new File(
-								Environment.getExternalStorageDirectory(),
-								Config.UPDATE_SAVENAME);
-						fileOutputStream = new FileOutputStream(file);
+    void downFile(final String url) {
+        pBar.show();
+        new Thread() {
+            public void run() {
+                HttpClient client = new DefaultHttpClient();
+                HttpGet get = new HttpGet(url);
+                HttpResponse response;
+                try {
+                    response = client.execute(get);
+                    HttpEntity entity = response.getEntity();
+                    long length = entity.getContentLength();
+                    InputStream is = entity.getContent();
+                    FileOutputStream fileOutputStream = null;
+                    if (is != null) {
 
-						byte[] buf = new byte[1024];
-						int ch = -1;
-						int count = 0;
-						while ((ch = is.read(buf)) != -1) {
-							fileOutputStream.write(buf, 0, ch);
-							count += ch;
-							if (length > 0) {
-							}
-						}
+                        File file = new File(
+                                Environment.getExternalStorageDirectory(),
+                                Config.UPDATE_SAVENAME);
+                        fileOutputStream = new FileOutputStream(file);
 
-					}
-					fileOutputStream.flush();
-					if (fileOutputStream != null) {
-						fileOutputStream.close();
-					}
-					down();
-				} catch (ClientProtocolException e) {
-					e.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
+                        byte[] buf = new byte[1024];
+                        int ch = -1;
+                        int count = 0;
+                        while ((ch = is.read(buf)) != -1) {
+                            fileOutputStream.write(buf, 0, ch);
+                            count += ch;
+                            if (length > 0) {
+                            }
+                        }
 
-		}.start();
+                    }
+                    fileOutputStream.flush();
+                    if (fileOutputStream != null) {
+                        fileOutputStream.close();
+                    }
+                    down();
+                } catch (ClientProtocolException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
 
-	}
+        }.start();
 
-	void down() {
-		handler.post(new Runnable() {
-			public void run() {
-				pBar.cancel();
-				update();
-			}
-		});
+    }
 
-	}
+    void down() {
+        handler.post(new Runnable() {
+            public void run() {
+                pBar.cancel();
+                update();
+            }
+        });
 
-	void update() {
+    }
 
-		Intent intent = new Intent(Intent.ACTION_VIEW);
-		intent.setDataAndType(Uri.fromFile(new File(Environment
-				.getExternalStorageDirectory(), Config.UPDATE_SAVENAME)),
-				"application/vnd.android.package-archive");
-		startActivity(intent);
-	}
+    void update() {
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setDataAndType(Uri.fromFile(new File(Environment
+                        .getExternalStorageDirectory(), Config.UPDATE_SAVENAME)),
+                "application/vnd.android.package-archive");
+        startActivity(intent);
+    }
 
 }
